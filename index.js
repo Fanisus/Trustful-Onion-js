df1c382e9a2865945bae39d998c6b6e828618403
Fanisus <09rfanisus@gmail.com>
2022-05-15 15:26:28 +0530

    Removed Translate due to security issues

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const db = new Database.Database('./Database', {
 // Weeeeeeeeeeeeeeeeeeeeeeeeeee
 const discord = require('discord.js');
 const Canvas = require('canvas');
-const translate = require('translate-google');
 const fs = require('fs');
 const client = new discord.Client({ intents: [discord.Intents.FLAGS.DIRECT_MESSAGES, discord.Intents.FLAGS.DIRECT_MESSAGE_REACTIONS, discord.Intents.FLAGS.DIRECT_MESSAGE_TYPING, discord.Intents.FLAGS.GUILDS, discord.Intents.FLAGS.GUILD_BANS, discord.Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS, discord.Intents.FLAGS.GUILD_INTEGRATIONS, discord.Intents.FLAGS.GUILD_INVITES, discord.Intents.FLAGS.GUILD_MEMBERS, discord.Intents.FLAGS.GUILD_MESSAGES, discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS, discord.Intents.FLAGS.GUILD_MESSAGE_TYPING, discord.Intents.FLAGS.GUILD_PRESENCES, discord.Intents.FLAGS.GUILD_VOICE_STATES, discord.Intents.FLAGS.GUILD_WEBHOOKS] });
 const moment = require('moment');
